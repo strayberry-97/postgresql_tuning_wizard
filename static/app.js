0f57944940db501e0948210eb7d3fcfbb1cf029f
Strayberry9 <109548625+strayberry-97@users.noreply.github.com>
2026-08-13 19:11:33 +0300

    Finish progression panel

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 
-document.getElementById('conn-next').addEventListener("click", () => {
+document.getElementById('connection-next').addEventListener("click", () => {
   changeSection('query');
 })
 
@@ -8,10 +8,29 @@ document.getElementById('query-back').addEventListener("click", () => {
   changeSection('connection');
 })
 
-function changeSection(section_name) {
+function changeSection(sectionName) {
   let x = document.querySelectorAll("section");
   x.forEach(section => {
     section.hidden = true;
   })
-  document.querySelector('#'+section_name).hidden = false;
+  document.querySelector('#'+sectionName).hidden = false;
+  updateStepper(sectionName);
+}
+
+function updateStepper(currentStep){
+  let x = document.querySelectorAll("#Progression .step");
+  x.forEach(y=>{
+      y.classList.remove("active");
+      y.classList.remove("completed")
+  })
+  let indexes = {"connection" : 0, "query" : 1, "analysis" : 2, "recommendations" : 3};
+  let currentIndex = indexes[currentStep];
+  x.forEach((y, index)=>{
+      if(index<currentIndex){
+        y.classList.add('completed');
+      } else if(index === currentIndex){
+        y.classList.add('active');
+      }
+
+  })
 }
